1a86c6b578c0c5ee725311a17a424c4b555d74db
Hassan Alkhalidi <5357529+Gruce@users.noreply.github.com>
2022-10-01 16:21:24 +0300

    Init

---
+++ bin/index.js
@@ -0,0 +1,31 @@
+#! /usr/bin/env node
+
+import figlet from "figlet";
+import gradient from "gradient-string";
+import consola from 'consola'
+import commands from '../src/commands.json' assert { type: "json" };
+import Table from 'cli-table'
+import * as cmdRunners from '../src/commands/index.js'
+
+
+/* Printing the name of Enab in the console. */
+figlet("E n a b", (err, data) => {
+    console.log(gradient.cristal(data));
+});
+
+await new Promise(resolve => setTimeout(resolve, 500));
+
+
+const command = commands.find(command => command.command == process.argv[2])
+if (!command){
+    consola.error('Please choose one of the following commands:')
+    const table = new Table({ head: ['COMMAND', 'DESCRIPTION'] });
+    table.push(...commands.map(cmd => [`enab ${cmd.command}`, cmd.title]))
+    console.log(table.toString());
+    process.exit(1);
+}
+
+// Process function with arguments
+cmdRunners[command.function]()
+
+
---
+++ src/commands/createPackage.js
@@ -0,0 +1,43 @@
+import inquirer from 'inquirer';
+import runner from './createPackageRun.js';
+
+async function promptForMissingOptions() {
+  const defaultTemplate = 'JavaScript';
+
+  const questions = [];
+    questions.push({
+      type: 'input',
+      name: 'name',
+      message: 'Please choose name of the package',
+    });
+
+    questions.push({
+      type: 'list',
+      name: 'template',
+      message: 'Please choose which project template to use',
+      choices: ['JavaScript', 'TypeScript'],
+      default: defaultTemplate,
+    });
+
+  // if (!options.git) {
+  //   questions.push({
+  //     type: 'confirm',
+  //     name: 'git',
+  //     message: 'Initialize a git repository?',
+  //     default: false,
+  //   });
+  // }
+
+  const answers = await inquirer.prompt(questions);
+  return {
+    name: answers.name,
+    template: answers.template,
+    git: answers.git,
+  };
+}
+
+export async function createPackage() {
+  let options = await promptForMissingOptions();
+  console.log(options);
+  // runner(options);
+}
